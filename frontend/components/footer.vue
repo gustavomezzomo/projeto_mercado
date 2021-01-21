13fe7d02ab3abca8612b34ee5ded24d3539424b3
Gustavo Mezzomo <gustavo.mezzomo@buser.com.br>
2021-01-21 14:56:53 -0300

    mudanças toolbar e centraliza footer

--- frontend/components/footer.vue
+++ frontend/components/footer.vue
@@ -1,6 +1,6 @@
 <template>
   <v-footer color="red" class="yellow--text" app>
-    <v-layout>
+    <v-layout justify-center align-center>
       <span>Mercado Santa Inês - Rua José Alves de Paiva 301 - Santa Inês 2</span>
     </v-layout>
   </v-footer>
