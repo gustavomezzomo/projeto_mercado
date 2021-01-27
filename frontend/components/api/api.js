8835a94f233482c23761620f22f499c13d403579
Gustavo <gustavo.loureiro@buser.com.br>
2021-01-27 11:12:48 -0300

    corrige mais detalhes

--- frontend/components/api/api.js
+++ frontend/components/api/api.js
@@ -1,35 +1,35 @@
 import axios from '~/helpers/axios'
-
 axios.defaults.xsrfHeaderName = 'X-CSRFToken'
 axios.defaults.xsrfCookieName = 'csrftoken'
 
 const api = {
-  login(username, password) {
+  login (username, password) {
     return post('/api/login', { username, password })
   },
-  logout() {
+  logout () {
     return post('/api/logout')
   },
-  whoami() {
+  whoami () {
     return get('/api/whoami')
   },
-  add_todo(newtask) {
+  add_todo (newtask) {
     return post('/api/add_todo', { new_task: newtask })
   },
-  list_todos() {
+  list_todos () {
     return get('/api/list_todos')
   },
-  image() {
-    return get('/api/image');
+  image () {
+    return get('/api/image')
+  }
 }
 
 export default api
 
-function get(url, params) {
-    return axios.get(url, { params })
+export function get (url, params) {
+  return axios.get(url, { params })
 }
 
-function post(url, params) {
+export function post (url, params) {
   const fd = new FormData()
   params = params || {}
   Object.keys(params).map((k) => {
