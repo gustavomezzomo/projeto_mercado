b5da7bd40c553849663af7b7b73a5925a5ea974f
Gustavo Mezzomo <gustavo.mezzomo@buser.com.br>
2021-01-29 14:47:55 -0300

    color maps icon

--- frontend/components/footer.vue
+++ frontend/components/footer.vue
@@ -2,7 +2,7 @@
   <v-footer color="red" class="yellow--text" app>
     <v-layout justify-center align-center>
       <span>Mercado Santa Inês - Rua José Alves de Paiva 301 - Santa Inês 2</span>
-      <v-btn icon color="black" href="https://www.google.com.br/maps/place/Mercado+Santa+Ines/@-23.1666265,-45.8064849,17z/data=!3m1!4b1!4m5!3m4!1s0x94cc4c412d8a682d:0x8f5a9f1e0466af5d!8m2!3d-23.1666265!4d-45.8042962" target="_blank">
+      <v-btn icon color="yellow" href="https://www.google.com.br/maps/place/Mercado+Santa+Ines/@-23.1666265,-45.8064849,17z/data=!3m1!4b1!4m5!3m4!1s0x94cc4c412d8a682d:0x8f5a9f1e0466af5d!8m2!3d-23.1666265!4d-45.8042962" target="_blank">
         <v-icon>mdi-google-maps</v-icon>
       </v-btn>
     </v-layout>
